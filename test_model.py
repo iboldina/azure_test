ea89b4bad17860d57143bf10bde9e01d09454ee7
Inna Boldina <chouba07@gmail.com>
2023-10-11 23:24:46 +0200

    test ajout

---
+++ test_model.py
@@ -0,0 +1,16 @@
+import pytest
+def test_output_format():
+    # Load the model from the joblib file
+    model = joblib.load('model.joblib')
+
+    # Load the test data from the CSV file
+    X_test = pd.read_csv('test_production.csv')
+
+    # Predict on the test data
+    y_pred = model.predict(X_test)
+
+    # Check if y_pred is not empty
+    assert len(y_pred) > 0, "The output is empty"
+
+    # Check if y_pred only contains expected class labels (e.g., 0 and 1)
+    assert set(y_pred) <= {0, 1}, "The output contains unexpected class labels"
